0ecddcccf564fb70a7256ca4a0f997ce503e87c8
jackkhor <jackkhor@gmail.com>
2022-06-09 00:00:58 +0800

    Create canvas_example.js

---
+++ canvas_example.js
@@ -0,0 +1,48 @@
+function example(canvas) {
+
+    // Create the app and start the update loop
+    const app = new pc.Application(canvas, {
+        mouse: new pc.Mouse(document.body),
+        touch: new pc.TouchDevice(document.body)
+    });
+
+    const assets = {
+        'statue': new pc.Asset('statue', 'container', { url: '/models/statue.glb' }),
+        'script': new pc.Asset('script', 'script', { url: '/orbit-camera.js' })
+    };
+
+    const assetListLoader = new pc.AssetListLoader(Object.values(assets), app.assets);
+    assetListLoader.load(() => {
+
+        // Create an entity hierarchy representing the statue
+        const statueEntity = assets.statue.resource.instantiateRenderEntity();
+        statueEntity.setLocalScale(0.07, 0.07, 0.07);
+        statueEntity.setLocalPosition(0, -0.5, 0);
+        app.root.addChild(statueEntity);
+
+        // Create a camera with an orbit camera script
+        const camera = new pc.Entity();
+        camera.addComponent("camera", {
+            clearColor: new pc.Color(0.4, 0.45, 0.5)
+        });
+        camera.addComponent("script");
+        camera.script.create("orbitCamera", {
+            attributes: {
+                inertiaFactor: 0.2 // Override default of 0 (no inertia)
+            }
+        });
+        camera.script.create("orbitCameraInputMouse");
+        camera.script.create("orbitCameraInputTouch");
+        app.root.addChild(camera);
+
+        // Create a directional light
+        const light = new pc.Entity();
+        light.addComponent("light", {
+            type: "directional"
+        });
+        app.root.addChild(light);
+        light.setLocalEulerAngles(45, 30, 0);
+
+        app.start();
+    });
+}
